b4fd480157492a71944092c75a33ce62ff4d1faa
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-08-22 19:23:32 +0700

    Release v0.001

--- lib/Acme/CPANModules/DiffWrappers.pm
+++ lib/Acme/CPANModules/DiffWrappers.pm
@@ -11,14 +11,14 @@ my $srclist = $Acme::CPANModules::CLI::Wrapper::UnixCommand::LIST;
 our $LIST = {
     summary => "Wrappers for the diff Unix command",
     entries => [
-        grep { $_->{'x.command'} eq 'rsync' } @{ $srclist->{entries} }
+        grep { $_->{'x.command'} eq 'diff' } @{ $srclist->{entries} }
     ],
 };
 
 1;
 # ABSTRACT:
 
-=head1 SEE ALSO
+=head1 prepend:SEE ALSO
 
 L<Acme::CPANModules::CLI::Wrapper::UnixCommand>, from which this list is
 derived.
